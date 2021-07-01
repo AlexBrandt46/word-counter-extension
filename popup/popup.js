f7862f59c22b3348b19315d1d107b05a5b7c2bb1
Alex Brandt <ajb3155@gmail.com>
2021-06-30 23:24:15 -0400

    Added word count functionality

--- popup/popup.js
+++ popup/popup.js
@@ -2,11 +2,15 @@ const wordCount = document.getElementById('word_count')
 const charCount = document.getElementById('char_count')
 const noSpaceCount = document.getElementById('no_space_count')
 const text_input = document.getElementById('text_input')
+let currentTab = "reeeee"
 
+// Set up code for text box
 text_input.addEventListener("input", function(event) {
-  var text = text_input.value
+  let text = text_input.value
+  wordCount.innerHTML = "Word Count: " + text.split(' ').length
   charCount.innerHTML = "Character Count: " + text.length
   text = text.replace(/\s+/g,'')
   noSpaceCount.innerHTML = "Character Count w/o Spaces: " + text.length
 })
+
 
